773cbfc4a8d834d6d7af0917d3828dae50ef980e
piyushkaware <piyushkaware3@gmail.com>
2022-02-03 20:08:31 +0530

    initial commit for react assignment 2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
 
 function App(props) {
- const {employee}=props;
+  const { employee } = props;
+
+  return (
+    <div className="container">
+      {/* Start your React code here */}
+      <div>
+      <img className='photo' src={employee.profileImg} alt='profile_image'></img>
+        <h1>{employee.name}</h1>
+        <p>Location</p>
+        <h3>{employee.location}</h3>
+        <p>Blood group</p>
+        <h3>{employee.bloodGroup}</h3>
+        <p>Age</p>
+        <h3>{employee.age}</h3>
+      </div>
     
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+      
+    </div>
+  )
 }
 
 
